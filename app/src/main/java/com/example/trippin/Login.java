1ca5cdd30209630676497d903154853413e271f6
Kapil Arora <kapilarora5252@gmail.com>
2021-03-14 17:53:07 +0530

    Login and Register

--- app/src/main/java/com/example/trippin/Login.java
+++ app/src/main/java/com/example/trippin/Login.java
@@ -1,15 +1,29 @@
 package com.example.trippin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import com.google.android.material.chip.Chip;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    private Button registerButton, loginButton, login;
+    private EditText editEmail, editPassword;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,18 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        registerButton = (Button) findViewById(R.id.registerButton);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(this);
+
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void loginFunc(View view) {
@@ -27,5 +53,56 @@ public class Login extends AppCompatActivity {
     public void registerFunc(View view) {
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.registerButton:
+                startActivity(new Intent(this, Register.class));
+                break;
+            case R.id.loginButton:
+                startActivity(new Intent(this, Login.class));
+            case R.id.login:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email = editEmail.getText().toString().trim();
+        String password = editPassword.getText().toString().trim();
+
+        if(email.isEmpty()) {
+            editEmail.setError("Email is required!");
+            editEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editEmail.setError("Please provide valid email!");
+            editEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            editPassword.setError("Password is required!");
+            editPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(Login.this, "You are now Logged In!!", Toast.LENGTH_SHORT).show();
+                    //redirect to user profile or home screen
+                }else {
+                    Toast.makeText(Login.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/trippin/Register.java
+++ app/src/main/java/com/example/trippin/Register.java
@@ -1,14 +1,40 @@
 package com.example.trippin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+
+public class Register extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+    EditText editName, editEmail, editPassword;
+    private ProgressBar progressBar;
+    Button registerUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +42,19 @@ public class Register extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registerUser = (Button) findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(this);
+
+        editName = (EditText) findViewById(R.id.editName);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
+
 
     public void loginFunc(View view) {
         Intent intent = new Intent(this, Login.class);
@@ -26,5 +64,84 @@ public class Register extends AppCompatActivity {
     public void registerFunc(View view) {
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginButton:
+                startActivity(new Intent(this, Login.class));
+                break;
+            case R.id.registerUser:
+                register();
+                break;
+        }
+
+    }
+
+    private void register() {
+        String email = editEmail.getText().toString().trim();
+        String password = editPassword.getText().toString().trim();
+        String name = editName.getText().toString().trim();
+
+        if(name.isEmpty()) {
+            editName.setError("Name is required!");
+            editName.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()) {
+            editEmail.setError("Email is required!");
+            editEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editEmail.setError("Please provide valid email!");
+            editEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            editPassword.setError("Password is required!");
+            editPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6) {
+            editPassword.setError("Min password length should be 6 characters");
+            editPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            User user = new User(name, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()) {
+                                        Toast.makeText(Register.this, "User has been registered successfully!", Toast.LENGTH_SHORT).show();
+
+                                    }else {
+                                        Toast.makeText(Register.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+
+                        }else {
+                            Toast.makeText(Register.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
     }
 }
